500a97d88a21407758812c68154d2cf8508386a6
fear-sfx <tmanoilov@abv.bg>
2012-10-18 09:22:04 +0300

    First bell task

--- belt1/belt1.rb
+++ belt1/belt1.rb
@@ -9,15 +9,15 @@ i = 0
 CSV.foreach(file_name) do |row|
 	x[i] = row[1].to_i
 	while y[i] < x[i] / 2
-		y[i] = rand(x[i])
+		y[i] = Random.rand(x[i])
 	end
 	i += 1
 end
 
 i = 0
-while i <50
-	CSV.open("export.csv", "ab") do |row|
-		row << [ i.to_s , x[i].to_s, y[i].to_s ]
-		i += 1
-	end
+	CSV.open("export.csv", "wb") do |row|
+		while i <50
+			row << [ i.to_s , x[i].to_s, y[i].to_s ]
+			i += 1
+		end
 end
