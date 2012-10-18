605bbd58ed93aad40c8ce17ea09b3ac79a223026
fear-sfx <tmanoilov@abv.bg>
2012-10-18 09:08:10 +0300

    First bell task

---
+++ belt1/belt1.rb
@@ -0,0 +1,23 @@
+require "csv"
+
+file_name = gets.chomp
+
+x = Array.new(50) {0}
+y = Array.new(50) {0}
+i = 0
+
+CSV.foreach(file_name) do |row|
+	x[i] = row[1].to_i
+	while y[i] < x[i] / 2
+		y[i] = rand(x[i])
+	end
+	i += 1
+end
+
+i = 0
+while i <50
+	CSV.open("export.csv", "ab") do |row|
+		row << [ i.to_s , x[i].to_s, y[i].to_s ]
+		i += 1
+	end
+end
